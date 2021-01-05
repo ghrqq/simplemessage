c5a07fe23a89416a3b96617dee19c3a79bbb78b2
Theo <theoozdev@gmail.com>
2021-01-05 18:09:36 +0100

    deploy

--- server.js
+++ server.js
@@ -110,8 +110,15 @@ app.get("/api/", (_req, res) => {
   res.send(`Hi `);
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 const PORT = process.env.PORT || 4000;
