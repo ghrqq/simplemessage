fe2a8efb2e6e447c6c1afd9debf24fbb24e57cd9
Theo <theoozdev@gmail.com>
2021-01-05 13:23:28 +0100

    Deploy config

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ app.get("/api/", (_req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.js"));
 });
 
 const PORT = process.env.PORT || 4000;
